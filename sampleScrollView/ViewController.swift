297405d842642a75079f27f44b9298ec92c189d6
yuka <yoogle8011@gmail.com>
2018-06-05 13:29:21 +0800

    Initial Commit

---
+++ sampleScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleScrollView
+//
+//  Created by yuka on 2018/06/05.
+//  Copyright © 2018年 yuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
